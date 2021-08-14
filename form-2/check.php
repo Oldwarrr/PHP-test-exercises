4f8fa769c4da28369191c4e80f6dbe238d130472
Mikhail Spirin <79533025370@yandex.ru>
2021-08-14 16:43:02 +0300

    update

--- form-2/check.php
+++ form-2/check.php
@@ -1,16 +1,40 @@
 <?php
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $tel = $_POST['tel'];
     $message = $_POST['message'];
     $choice = $_POST['choice'];
 }
-foreach($_POST as $name => $field){
-    $arFields[$field] = htmlspecialchars(stripslashes(strip_tags((trim($field)))));
-    echo "$name : $field" . "<br>" . "<hr>";
-    $fopen = fopen('text.txt', 'a+');
-    $fwrite = fwrite($fopen, "$name : $field \n");
-    $fclose = fclose($fopen);
-    
-}
 
+
+
+function check_length($value, $min, $max){ // Функция на проверку длины
+    $result = mb_strlen($value) < $min || mb_strlen($value) > $max;
+    return !$result;
+}
+if(!empty($tel) && !empty($message) && !empty($choice)){ // Заполненность полей
+
+    if(check_length($message, 5, 200)){ // Длина сообщения
+
+        foreach($_POST as $name => $field){ // Перебор элементов с выводом на экран
+            $arFields[$field] = htmlspecialchars(stripslashes(strip_tags((trim($field))))); // Очистка
+            echo "$name : $field" . "<br>" . "<hr>";
+            $fopen = fopen('data.txt', 'a+'); // Запись данных в файл
+            $fwrite = fwrite($fopen, "$name : $field \n");
+             $fclose = fclose($fopen);    
+        }
+    }else{
+        echo "Слишком короткое сообщение";
+    }
+  }else{
+      echo "Заполните все поля формы <br/>";
+  }
+
+
+  if(isset($_POST['click'])){
+    $fread = fopen("data.txt", "r");
+    $contents = fread($fread, filesize("data.txt"));
+    fclose($fread);
+    echo $contents;
+}
 
